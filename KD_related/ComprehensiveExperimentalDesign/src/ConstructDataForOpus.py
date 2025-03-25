2415fccf9519d834762ef6b340a53344ff6dca52
lh <skyfurynowonline@icloud.com>
2025-03-25 15:17:50 +0800

    修改加载重构数据后得到的数据类型

--- KD_related/ComprehensiveExperimentalDesign/src/ConstructDataForOpus.py
+++ KD_related/ComprehensiveExperimentalDesign/src/ConstructDataForOpus.py
@@ -2,7 +2,9 @@
 
 # 构造opus_books翻译数据集
 from dataset import train_opus_dataset,val_opus_dataset
-# 处理整个train数据集
+from datasets import Dataset
+
+# 处理整个 train 数据集
 alpaca_dataset = []
 for item in train_opus_dataset:
     alpaca_format = {
@@ -11,8 +13,11 @@ for item in train_opus_dataset:
         "output": item['translation']['en']
     }
     alpaca_dataset.append(alpaca_format)
-train_opus_dataset = alpaca_dataset
-# 处理整个val数据集
+
+# 转换为 Dataset 类型
+train_opus_dataset = Dataset.from_list(alpaca_dataset)
+
+# 处理整个 val 数据集
 alpaca_dataset = []
 for item in val_opus_dataset:
     alpaca_format = {
@@ -21,11 +26,6 @@ for item in val_opus_dataset:
         "output": item['translation']['en']
     }
     alpaca_dataset.append(alpaca_format)
-val_opus_dataset = alpaca_dataset
 
-print(train_opus_dataset[0])
-print(val_opus_dataset[0])
-'''
-{'instruction': 'Translate the following text from English to French.', 'input': 'Le grand Meaulnes', 'output': 'The Wanderer'}
-{'instruction': 'Translate the following text from English to French.', 'input': "Il remerciait presque sa femme d'être allée chez un amant lorsqu'il croyait qu'elle se rendait chez un commissaire de police.", 'output': 'He almost thanked his wife for having gone to a sweetheart, when he thought her on her way to a commissary of police.'}
-'''
+# 转换为 Dataset 类型
+val_opus_dataset = Dataset.from_list(alpaca_dataset)
