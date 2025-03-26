29d0c09ea8de3d9672c0654898b8d5f041c6c59a
lh <skyfurynowonline@icloud.com>
2025-03-26 19:54:18 +0800

    完成对qa数据集的处理

---
+++ KD_related/ComprehensiveExperimentalDesign/src/ConstructDataForQA.py
@@ -0,0 +1,32 @@
+# 构造qa数据集
+from dataset import train_qa_dataset,val_qa_dataset
+from datasets import Dataset
+
+# 处理整个 train 数据集
+alpaca_dataset = []
+for item in train_qa_dataset:
+    alpaca_format = {
+        "instruction": "Please read the following article and answer the question based on its content.",
+        "input": "Article: "+item['context']+"\n\nQuestion: "+item['question'],
+        "output":item['answers']['text'][0]
+    }
+    alpaca_dataset.append(alpaca_format)
+
+# 转换为 Dataset 类型
+train_qa_dataset = Dataset.from_list(alpaca_dataset)
+
+# 处理整个 val 数据集
+alpaca_dataset = []
+for item in val_qa_dataset:
+    alpaca_format = {
+        "instruction": "Please read the following article and answer the question based on its content.",
+        "input":"Article: "+item['context']+"\n\nQuestion: "+item['question'] ,
+        "output":item['answers']['text'][0]
+    }
+    alpaca_dataset.append(alpaca_format)
+
+# 转换为 Dataset 类型
+val_qa_dataset = Dataset.from_list(alpaca_dataset)
+
+# {'instruction': 'Please read the following article and answer the question based on its content.', 'input': 'Article: Architecturally, the school has a Catholic character. Atop the Main Building\'s gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend "Venite Ad Me Omnes". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.\n\nQuestion: To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?', 'output': 'Saint Bernadette Soubirous'}
+
--- KD_related/ComprehensiveExperimentalDesign/src/dataset.py
+++ KD_related/ComprehensiveExperimentalDesign/src/dataset.py
@@ -57,3 +57,9 @@ Dataset({
 # 加载总结的数据集
 train_summary_dataset = load_dataset("abisee/cnn_dailymail",'3.0.0',cache_dir="../data",split='train[:2000]')
 val_summary_dataset = load_dataset("abisee/cnn_dailymail",'3.0.0',cache_dir='../data',split='train[-200:]')
+
+
+# 加载QA数据集
+train_qa_dataset = load_dataset("rajpurkar/squad",cache_dir="../data",split='train[:2000]')
+val_qa_dataset = load_dataset("rajpurkar/squad",cache_dir="../data",split='train[-200:]')
+# {'id': '5733be284776f41900661182', 'title': 'University_of_Notre_Dame', 'context': 'Architecturally, the school has a Catholic character. Atop the Main Building\'s gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend "Venite Ad Me Omnes". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.', 'question': 'To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?', 'answers': {'text': ['Saint Bernadette Soubirous'], 'answer_start': [515]}}
